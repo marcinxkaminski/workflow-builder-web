cb6770a8482f9413b71baefef80ffa54ab5ce4b4
Marcin Kamiński <Marcin.Kaminski@dreamlab.pl>
2019-08-04 22:33:23 +0200

    🐛 Fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'react',
   ],
   rules: {
+    "react/prefer-stateless-function": "off"
   },
   overrides: [
     {
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,8 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-
+        Hi!
       </div>
-    )
+    );
   }
 }
