de8bac4479b7c695b382ed0564ccd313e87b51f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-05 19:25:08 +0000

    Deploy xkamson/ml-web to github.com/xkamson/ml-web.git:gh-pages

---
+++ precache-manifest.a40b1697bca1a2f100fb314561b94945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61baa387f0ff11b543c7f6e1d78586ea",
+    "url": "/ml-web/index.html"
+  },
+  {
+    "revision": "89589c1fddc3196c13fb",
+    "url": "/ml-web/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ml-web/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "89589c1fddc3196c13fb",
+    "url": "/ml-web/static/js/main.edf38dfc.chunk.js"
+  },
+  {
+    "revision": "8bc0371150ded40163ac",
+    "url": "/ml-web/static/js/runtime~main.a5d6bb10.js"
+  }
+]);
