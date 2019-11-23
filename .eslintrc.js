98c9bde10c50583a79981a42e2717f55b841aa64
Marcin Kaminski <marcin.kaminski@dreamlab.pl>
2019-11-23 21:49:37 +0100

    🐛Fixed CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,7 @@ module.exports = {
   ],
   rules: {
     "import/prefer-default-export": "off",
-    "no-console": ["error", { allow: ["warn", "error"] }],
-    "import/no-unresolved": "off"
+    "no-console": ["error", { allow: ["warn", "error"] }]
   },
   overrides: [
     {
