a0433002333b121780a5b6e535a466b52c3adb6f
Marcin Kaminski <marcin.kaminski@dreamlab.pl>
2019-11-23 20:17:21 +0100

    🐛 Trying fix CI

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import App from './App/App';
+import App from 'App/App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.css';
