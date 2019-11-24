959d205b413a6c361130a8d65b9c099ac9f261ad
Marcin Kaminski <marcin.kaminski@dreamlab.pl>
2019-11-24 18:40:05 +0100

    🐛 Fixed Api endpoint

--- src/api/apiEndpoints.js
+++ src/api/apiEndpoints.js
@@ -1,3 +1,3 @@
-export const BASE_API_URL = 'https://workflow-builder-api.herokuapp.com/';
+export const BASE_API_URL = 'https://workflow-builder-api.herokuapp.com/api/workflow-builder';
 export const WORKFLOW_ELEMENTS = '/elements';
 export const WORKFLOW_FILES = '/files';
